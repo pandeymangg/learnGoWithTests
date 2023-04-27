c4204aada7cb68b80cf16c429fdbea7d5ca37ebb
= <anshuman@tealfeed.com>
2023-04-27 22:35:02 +0530

    language functionality

--- hello.go
+++ hello.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+const hindi = "Hindi"
+const englishHelloPrefix = "Hello, "
+const hindiHelloPrefix = "Namaste, "
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		return "Hello, World!"
+	}
+
+	if lang == hindi {
+		return hindiHelloPrefix + name + "!"
+	}
+
+	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("Anshuman"))
+	fmt.Println(Hello("Anshuman", "Hindi"))
 }
