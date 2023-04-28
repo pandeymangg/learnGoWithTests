d290bced488e7c47372b3972733b12cdca8260ec
pandeymangg <anshuman.pandey9999@gmail.com>
2023-04-28 22:17:46 +0530

    arrays and slices main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	arraysandslices "github.com/pandeymangg/learnGoWithTests/arraysAndSlices"
 	"github.com/pandeymangg/learnGoWithTests/helloWorld"
 	"github.com/pandeymangg/learnGoWithTests/integers"
 	"github.com/pandeymangg/learnGoWithTests/iteration"
@@ -14,5 +15,12 @@ func main() {
 	fmt.Println(integers.Add(2, 20))
 
 	fmt.Println(iteration.Repeat("c", 69))
+
+	slice1 := []int{1, 2, 3}
+	slice2 := []int{1, 2}
+
+	sliceSum := arraysandslices.SumAllTails(slice1, slice2)
+
+	fmt.Println(sliceSum)
 
 }
