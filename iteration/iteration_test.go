47dbc295ecadd2322763ac39c6a43c0d0cefa81d
= <anshuman@tealfeed.com>
2023-04-28 19:25:15 +0530

    feat: testing iterations

---
+++ iteration/iteration_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q, got %q", expected, repeated)
+	}
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 6)
+	}
+}
+
+func ExampleRepeat() {
+	output := Repeat("b", 3)
+	fmt.Println(output)
+	//Output: bbb
+}
